58158bbfbb4708caa631d37b561b5649277e00e1
Ricardo Simone <111938106+ricksimonsp@users.noreply.github.com>
2022-11-14 22:19:23 -0500

    More Functions

--- 03JavaScript/js04/script/functions.js
+++ 03JavaScript/js04/script/functions.js
@@ -1,15 +1,33 @@
-
 //FUNCTIONS
-//Criar um aplicativos de frases motivacionais
+//Declaration - declaração da função
+//Function statement
 
-function createPhrases() {
+/* function creatPhrase () {
     console.log('Estudar é muito bom!');
     console.log('Paciência e persistência!');
-    console.log('Revisão é mãe do aprendizado!');
+    console.log('Revisão é mãe do aprendizado');
 }
 
 //execute, run, call, invoke
 
-createPhrases()
+creatPhrase()
+console.log('Fim do Programa!') */
+//FUNCTION EXPRESSION
+//FUNCTION ANONYMOUS
 
-console.log('Fim do programa!')
+//parâmetros - parameters
+const sum = function(number1, number2) {
+    total = number1 + number2
+    return total
+
+}
+
+//arguments
+let number1 = 34
+let number2 = 25
+sum(number1, number2)
+
+console.log(`o número 1 é ${number1}`)
+console.log(`o número 2 é ${number2}`)
+console.log(`a soma é ${sum(number1, number2)}`)
+
