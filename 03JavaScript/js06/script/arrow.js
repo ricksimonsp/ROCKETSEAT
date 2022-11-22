0da4028253c777d5d026b9db8ea5699f3b477693
Ricardo Simone <ricardo.simone2@gmail.com>
2022-11-22 19:47:06 -0300

    Ok for today

---
+++ 03JavaScript/js06/script/arrow.js
@@ -0,0 +1,48 @@
+//Arrow Function
+/*
+const sayMyName = () => {
+    console.log('Ricardo')
+}
+
+sayMyName()
+*/
+
+//Callback Function
+
+/*function sayMyName(name){
+    console.log('Antes de executar a função callback!')
+
+    name()
+
+    console.log('Depois de executar a callback!')
+}
+
+sayMyName(
+    () => {
+        console.log('Estou em uma callback!')
+    }
+)
+*/
+
+
+
+/*
+
+Function Constructor
+  * Expressão New
+  * Criar um novo objeto
+  * This keyword
+
+*/
+
+function Person(name) {
+    this.name = name
+    this.walk = function() {
+        return this.name + ' está andando'
+    }
+}
+
+const ric = new Person('Ricardo')
+const ro = new Person('Rosangela')
+console.log(ric.walk())
+console.log(ro.walk())
