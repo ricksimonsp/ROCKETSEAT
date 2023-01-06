ec906e9f8706c23adfa2553517ef117adc20e0c9
Ricardo Simone <111938106+ricksimonsp@users.noreply.github.com>
2023-01-05 21:43:59 -0500

    Get ready for practicing!

--- 03JavaScript/js13/script/script.js
+++ 03JavaScript/js13/script/script.js
@@ -1,6 +1,53 @@
 // Estrutura de repetição
 // For
+// break - faz parar a execução do loop
+// continue - pula a execução do momento
 
-for(let i = 0; i < 10; i++) {
-    
+for(let e = 15; e > 0; e--) {
+    if(e === 10) {
+        break;
+    }
+    console.log(e)   
 }
+
+
+for(let i = 10; i > 0; i--) {
+    if(i === 5) {
+        continue;
+    }
+    console.log(i)   
+}
+
+
+// while (por enquanto)
+
+let a = 54375843350;
+while(a > 10) {
+    console.log(a)
+
+    a /= 100
+}
+
+
+// for...of
+
+let name = "Rick";
+let names = ["Rosangela", "Fernanda", "Adrielli"];
+
+for(let name of names) {
+    console.log(name)
+}
+
+// for...in
+
+let person = {
+    nickname: "Rick",
+    age: 49,
+    weight: 85
+}
+
+for (let property in person) {
+    console.log(property)
+    console.log(person[property])
+}
+
